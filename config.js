0270c4f222e8e95f05e17e12a72c9d43fea25801
ALPHAkaveen <103016362+ALPHAkaveen@users.noreply.github.com>
2022-12-20 12:59:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ global.devs = '919628516236';
 global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
-  botname: process.env.BOT_NAME || '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨',
+  botname: process.env.BOT_NAME || 'Dark Nero',
   ownername:process.env.OWNER_NAME || 'SamPandey001',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
